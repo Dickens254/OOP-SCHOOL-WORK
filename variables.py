4921bc91d5aec99d93dc2ced03ba93f2ac69e157
Dickens254 <77730314+Dickens254@users.noreply.github.com>
2021-01-27 10:19:01 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,10 @@
+#variables are containers for storing data types
+# 1.integer variables
+y = 10
+x = 5
+print (y + x) #prints out the sum of y and x
+
+#2. string variables
+t = 'Dickens'
+m = ' ochieng'
+print (t + m)# joins the strings t and m
